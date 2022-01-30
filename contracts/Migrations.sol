693f52bbd81be975f2256063179f6e22ff494554
chrisais9 <chrisais9@naver.com>
2022-01-31 01:41:17 +0900

    add HelloWorld Contract / ignore build file

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -2,18 +2,34 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Migrations {
-  address public owner = msg.sender;
-  uint public last_completed_migration;
+    address public owner = msg.sender;
+    uint256 public last_completed_migration;
 
-  modifier restricted() {
-    require(
-      msg.sender == owner,
-      "This function is restricted to the contract's owner"
-    );
-    _;
-  }
+    modifier restricted() {
+        require(
+            msg.sender == owner,
+            "This function is restricted to the contract's owner"
+        );
+        _;
+    }
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
+    function setCompleted(uint256 completed) public restricted {
+        last_completed_migration = completed;
+    }
+}
+
+contract HelloWorld {
+    string public greeting;
+
+    constructor(string memory _greeting) {
+        greeting = _greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+        greeting = _greeting;
+    }
+
+    function say() public view returns (string memory) {
+        return greeting;
+    }
 }
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,10 @@
 const Migrations = artifacts.require("Migrations");
+const HelloWorld = artifacts.require("HelloWorld");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
 };
+
+module.exports = function (deployer) {
+  deployer.deploy(HelloWorld);
+}
