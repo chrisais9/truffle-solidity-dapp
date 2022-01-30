772d3b25abdd0939e7edb7c05e51c5356264fb33
chrisais9 <chrisais9@naver.com>
2022-01-31 01:56:14 +0900

    parse contract files with correct order

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -17,19 +17,3 @@ contract Migrations {
         last_completed_migration = completed;
     }
 }
-
-contract HelloWorld {
-    string public greeting;
-
-    constructor(string memory _greeting) {
-        greeting = _greeting;
-    }
-
-    function setGreeting(string memory _greeting) public {
-        greeting = _greeting;
-    }
-
-    function say() public view returns (string memory) {
-        return greeting;
-    }
-}
